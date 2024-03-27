8fdc50f431445c14706a56fa803551912c2f425c
Paras Gogdani <parasgogdani027@gmail.com>
2024-03-27 18:50:05 +0530

    '-paras'

--- README.md
+++
@@ -1,4 +0,0 @@
-#dream-team-11-admin
-
-ng generate module shop --route shop --module app.module
-"# dream-team-11-admin" 
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  serverURL: 'http://localhost:3000/v1/',
-  imageURL: 'http://localhost:3000/v1/'
-};
